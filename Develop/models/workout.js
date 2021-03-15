235f33a7416510c8fca9567c1ae2437e810b6456
Vivianne Cardillo <viviannecardillo@Viviannes-MBP.fios-router.home>
2021-03-15 18:04:39 -0400

    build out the schema instance in workout.js and fixed the htmlRoutes.js file that were not rendering due to incorrect app.get() order of routes that were skipping over eachother.

--- Develop/models/workout.js
+++ Develop/models/workout.js
@@ -10,7 +10,48 @@ const Schema = mongoose.Schema;
 //  - todo next, build out the instance of the schema now
 // review front end to see how data is populating the view
 const WorkoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: () => Date()
+    },
+    exercises: [
+        {
+            weight: {
+                type: Number,
+                default: 0
+            },
+            reps: {
+                type: Number,
+                default: 0
+            },
+            sets: {
+                type: Number,
+                default: 0
+            },
+            distance: {
+                type: Number,
+                default: 0
+            },
+            type:{
+                type: String,
+                trim: true,
+                required: true
+            },
+            name: {
+                type: String,
+                trim: true,
+                required: true,
+                default: "workout exercise"
 
+            },
+            duration: {
+                type: Number,
+                required: true,
+                default: 0
+
+            }            
+        }
+    ]
     });
 
 // export our created mongoose model from WorkourSchema to be used elsewhere
